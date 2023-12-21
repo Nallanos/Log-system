29f534a42565781a6081ec48b7c67dff54b7c115
Nallanos <123402313+Nallanos@users.noreply.github.com>
2023-12-21 17:21:55 +0000

    Sync

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -30,6 +30,7 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart />
         <Suspense fallback={<InvoiceSkeleton />}>
         </Suspense>
         <LatestInvoices />
