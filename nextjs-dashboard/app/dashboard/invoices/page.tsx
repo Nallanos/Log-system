45911439adae45959e0e92bec9f6d7935ee659bd
Nallanos <123402313+Nallanos@users.noreply.github.com>
2023-12-28 14:07:13 +0000

    commit

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-
+import { fetchInvoicesPages } from '@/app/lib/data';
 export default async function Page({
   searchParams,
 }: {
@@ -16,7 +16,8 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-
+  const totalPages = await fetchInvoicesPages(query);
+  console.log(totalPages)
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
